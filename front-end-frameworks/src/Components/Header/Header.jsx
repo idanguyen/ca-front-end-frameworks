bad71f5c30d2725ea91419592c427fab98b25b9c
Ida Nguyen <115324491+idanguyen@users.noreply.github.com>
2024-04-14 23:52:10 +0200

    Fix eslint

--- front-end-frameworks/src/Components/Header/Header.jsx
+++ front-end-frameworks/src/Components/Header/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Header.css";
 import cart from "../Assets/cart.png";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 
 const Header = (items) => {
   return (
